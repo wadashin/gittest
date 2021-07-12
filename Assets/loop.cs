b1e38956ff3949ed538ce7378b8841e14630c894
wadashin <Shin.86.miso@outlook.com>
2021-07-12 14:18:15 +0900

    loopスクリプト追加

---
+++ Assets/loop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loop : MonoBehaviour
+{
+    //public int a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int a = 0; a <= 100; a++)
+        {
+            Debug.Log(a);
+        }
+    }
+}
